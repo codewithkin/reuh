0e39ef2834b27a7de767a5d5824c1e3748a1af29
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-10 13:36:23 +0200

    Layout updates

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import RecentActivity from "@/components/dashboard/RecentActivity";
 import { Button } from "@/components/ui/button";
 import { ArrowUp, ArrowUpFromDotIcon } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Dashboard"
@@ -13,12 +14,12 @@ export const metadata: Metadata = {
 export default function Dashboard () {
 
   return (
-    <section className="py-4 overflow-y-scroll h-screen md:py-8 md:px-4 w-full">
+    <section className="py-4 overflow-y-scroll h-screen md:py-0 md:pb-8 md:px-4 w-full">
       {/* Top Bar */}
       <article className="flex flex-col w-full md:flex-row gap-2 md:justify-between md:items-center">
         <article className="flex gap-2 items-center">
           <img src="/images/design/suit.jpg" className="w-8 h-8 rounded-full flex md:hidden" alt="me" />
-          <h2 className="font-semibold text-xl md:text-2xl">Welcome back, Kin Leon !</h2>
+          <h2 className="font-semibold text-2xl">Welcome back, Kin Leon !</h2>
         </article>
 
         {/* Buttons and Avatar */}
@@ -45,7 +46,10 @@ export default function Dashboard () {
               0
             </h3>
 
-            <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-secondaryLight px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+            <article className="flex gap-2 justify-center items-center">
+              <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-secondaryLight px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+              <Link className="bg-primaryDark rounded-full text-white py-2 px-4" href="/">New</Link>
+            </article>
         </article>
 
         <article className="mx-8 md:mx-0 w-full even:bg-primaryLight rounded-2xl p-4 text-white font-semibold bg-dullDark flex flex-col gap-4">
@@ -55,7 +59,11 @@ export default function Dashboard () {
               0
             </h3>
 
-            <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-tertiary px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+            <article className="flex gap-2 justify-center items-center">
+              <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-tertiary px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+              <Link className="bg-primaryDark rounded-full text-white py-2 px-4" href="/">New</Link>
+             
+            </article>
         </article>
 
         <article className="mx-8 md:mx-0 w-full even:bg-primaryLight rounded-2xl p-4 text-white font-semibold bg-dullDark flex flex-col gap-4">
@@ -65,7 +73,11 @@ export default function Dashboard () {
               0
             </h3>
 
-            <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-secondaryLight px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+            <article className="flex gap-2 justify-center items-center">
+              <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-secondaryLight px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+              <Link className="bg-primaryDark rounded-full text-white py-2 px-4" href="/">New</Link>
+             
+            </article>
         </article>
         <article className="mx-8 md:mx-0 w-full even:bg-primaryLight rounded-2xl p-4 text-white font-semibold bg-dullDark flex flex-col gap-4">
             <h2 className="text-xl">Test Interviews</h2>
@@ -74,7 +86,10 @@ export default function Dashboard () {
               0
             </h3>
 
-            <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-danger px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+            <article className="flex justify-center gap-2 items-center">
+              <article className="hover:cursor-pointer transition duration-400 hover:bg-primaryDark flex shadow shadow-xl items-center gap-2 bg-danger px-8 py-2 rounded-full text-center justify-center items-center">40% <ArrowUp /></article>
+              <Link className="bg-primaryDark rounded-full text-white py-2 px-4" href="/">New</Link>
+            </article>
         </article>
       </article>
 
